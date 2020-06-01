101edebcec9662b71adf936227820fc5a35def33
alv-bsp <alv@bendingspoons.com>
2020-06-01 17:33:48 +0200

    fix: favicon gen fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -151,7 +151,7 @@ module.exports = {
       // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
       background: "#fff",
       // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-      title: "WEBSITETITLE",
+      title: "Immuni",
 
       // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
       icons: {
