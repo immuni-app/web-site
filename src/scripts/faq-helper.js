33bbf1c293579dd1384386a3ab224d472bc7cee6
alv-bsp <alv@bendingspoons.com>
2020-06-08 17:36:24 +0200

    feat: fetch FAQ from CDN WIP

--- src/scripts/faq-helper.js
+++ src/scripts/faq-helper.js
@@ -1,33 +1,28 @@
 import gsap, { Expo } from "gsap";
 import { withinViewport } from "./reveal-helper";
 import * as imgUrl from "../assets/img/search-illustration.png";
-import faqIt from "../res/faq/faq_it.json";
-import faqEn from "../res/faq/faq_en.json";
-import faqDe from "../res/faq/faq_en.json";
-import faqEs from "../res/faq/faq_en.json";
-import faqFr from "../res/faq/faq_en.json";
-import faqPt from "../res/faq/faq_en.json";
-const resources = {
-  it: faqIt,
-  en: faqEn,
-  de: faqEn,
-  es: faqEn,
-  fr: faqEn,
-  pt: faqEn,
-};
 
 const empty = {
   it: "Nessun risultato trovato",
   en: "No matches found",
-  de: "de",
-  es: "es",
-  fr: "fr",
-  pt: "pt",
+  de: "Kein Ergebnis gefunden",
+  fr: "Aucun résultat trouvé",
+  es: "No se han encontrado resultados",
 };
 
 export function handleFaq() {
   const lang = localStorage.getItem("language");
-  const faq = resources[lang];
+  let faq = {};
+  fetch(`https://get.immuni.gov.it/docs/faq-${lang}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      faq = data;
+      console.log(faq);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   const list = document.querySelector(".accordion-wrapper");
   const search = document.querySelector(".search__input");
   const clearBtn = document.querySelector(".search__clear");
@@ -39,13 +34,13 @@ export function handleFaq() {
         // Commented until we have the categories back
         // li += `<h5 class="tagline tagline__faq"><div class="text-wrapper revealer">${el}</div></h5>`;
         elements[el].forEach((value) => {
-          li += `<div class="accordion"><button class="accordion__button">${value.title}</button><div class="accordion__panel">${value.content}</div></div>`;
+          li += `<div class="accordion"><button class="accordion__button">${value.title}</button><div class="accordion__panel"><p>${value.content}</p></div></div>`;
         });
       });
     } else {
       li = `<div class="no-results revealer"><p>${empty[lang]}</p><img src="${imgUrl.default}" alt="Illustration of two text bubbles saying nothing found" class="search-illustration"/></div>`;
     }
-    list.innerHTML = li;
+    list.innerHTML = li.split("\n").join("<br />");
 
     withinViewport();
     const animateList = [...document.querySelectorAll(".accordion")];
@@ -64,7 +59,7 @@ export function handleFaq() {
   renderFaq(faq);
   handleAccordion();
 
-  const filterList = (event) => {
+  const filterList = () => {
     let keyword = search.value.toLowerCase();
     clearBtn.style.visibility = keyword.length ? "visible" : "hidden";
     const filterQuestions = (list) => {
